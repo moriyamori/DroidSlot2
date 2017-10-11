2aab37b0a7eb3f19191f46f4053d2bd5bcdb1f3f
moriya <amamori6+s@gmail.com>
2017-10-11 17:33:55 +0900

    ３択画像表示

--- app/src/main/java/com/example/sample/droidslot/DroidSlotActivity.java
+++ app/src/main/java/com/example/sample/droidslot/DroidSlotActivity.java
@@ -3,6 +3,11 @@ package com.example.sample.droidslot;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.Random;
+
 
 public class DroidSlotActivity extends AppCompatActivity {
 
@@ -12,6 +17,22 @@ public class DroidSlotActivity extends AppCompatActivity {
         setContentView(R.layout.activity_droid_slot);
         //変数=指定したウィジェットID
         ImageView droidImage1 = (ImageView) this.findViewById(R.id.droidimageid1);
-        droidImage1.setImageResource(R.drawable.droid_back);
+        int droidSide1;
+
+        Random r = new Random();//Randomクラスのオブジェクト作成
+        droidSide1 = r.nextInt(4);//０以上２未満の整数乱数を作成
+        switch (droidSide1) {//向きを表す値が0であれば
+            case 0:
+                droidImage1.setImageResource(R.drawable.droid_front);
+                break;
+            case 1:
+                droidImage1.setImageResource(R.drawable.droid_back);
+                break;
+            case 2:
+                droidImage1.setImageResource(R.drawable.droid_left);
+                break;
+            case 3:
+                droidImage1.setImageResource(R.drawable.droid_right);
+        }
     }
 }
